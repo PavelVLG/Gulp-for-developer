e69ebc73a94d4d09120859fcf69d05f735f4d355
PavelVLG <pavelvlg4@mail.ru>
2021-03-23 17:45:52 +0300

    master

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,16 @@
-const { src, dest, series, watch, parallel } = require("gulp");
-const scss = require("gulp-sass");
-const csso = require("gulp-csso");
-const htmlmin = require("gulp-htmlmin");
-const del = require("del");
-const concat = require("gulp-concat");
-const autoprefixer = require("gulp-autoprefixer");
-const sync = require("browser-sync").create();
-const uglify = require("gulp-uglify-es").default;
-const plumber = require("gulp-plumber");
-const sourcemaps = require("gulp-sourcemaps");
-const rename = require("gulp-rename");
-const mediagroup = require("gulp-group-css-media-queries");
+const { src, dest, series, watch, parallel } = require("gulp"); // методы галпа
+const scss = require("gulp-sass"); //scss в css
+const csso = require("gulp-csso"); // сжимает css
+const htmlmin = require("gulp-htmlmin"); // сжимает html
+const del = require("del"); // удаление
+const concat = require("gulp-concat"); // обьединяет файлы в один
+const autoprefixer = require("gulp-autoprefixer"); // префиксы под браузеры
+const sync = require("browser-sync").create(); // локальный сервер
+const uglify = require("gulp-uglify-es").default; // сжимает js
+const plumber = require("gulp-plumber"); // показывает ошибки
+const sourcemaps = require("gulp-sourcemaps"); // делает map
+const rename = require("gulp-rename"); // переименовывает
+const mediagroup = require("gulp-group-css-media-queries"); // собирает медиа запросы и обединяет
 
 const server = () => {
     sync.init({
@@ -29,7 +29,7 @@ const path = {
         source: "dist" + "/" + "source",
     },
     dev: {
-        css: "src" + "/" + "scss" + "/**" + "/*" + ".scss",
+        scss: "src" + "/" + "scss" + "/*" + ".scss",
         js: "src" + "/" + "js" + "/*" + ".js",
         html: "src" + "/" + "index.html",
         source: {
@@ -42,11 +42,9 @@ const path = {
 };
 /***************************************************************/
 const html = () => {
-    return (
-        src(path.dev.html)
-            .pipe(htmlmin({ collapseWhitespace: true })) // Сжимает html.
-            .pipe(dest(path.build.html))
-    );
+    return src(path.dev.html)
+        .pipe(htmlmin({ collapseWhitespace: true })) // Сжимает html.
+        .pipe(dest(path.build.html));
 };
 /*****************************************************************/
 const js = () => {
@@ -68,7 +66,7 @@ const js = () => {
 };
 
 const jsLibr = () => {
-    return src([""])
+    return src(path.dev.source.ui)
         .pipe(plumber())
         .pipe(concat("vendor.min.js"))
         .pipe(uglify())
@@ -76,7 +74,7 @@ const jsLibr = () => {
 };
 
 const styles = () => {
-    return src("src/scss/*.scss")
+    return src(path.dev.scss)
         .pipe(sourcemaps.init())
         .pipe(plumber())
         .pipe(scss({ outputStyle: "expanded" }))
@@ -106,7 +104,7 @@ const source = () => {
     return src(path.dev.source.png).pipe(dest(path.build.source));
 };
 const watching = () => {
-    watch(path.dev.css, styles);
+    watch(path.dev.scss, styles);
     watch(path.dev.js, js);
     watch(path.dev.html).on("change", sync.reload);
     watch(path.dev.source.png, source);
